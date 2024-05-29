811032826a726bfa6b817423dc313437bf347083
ezmhme <emzhemingway@gmail.com>
2024-05-29 16:06:53 +1200

    updates

--- sketch.js
+++ sketch.js
@@ -2,8 +2,8 @@ let sourceImg=null;
 let maskImg=null;
 
 // change these three lines as appropiate
-let sourceFile = "input_1.jpg";
-let maskFile   = "mask_1.png";
+let sourceFile = "input_2.jpg";
+let maskFile   = "mask_2.png";
 let outputFile = "output_1.png";
 
 function preload() {
